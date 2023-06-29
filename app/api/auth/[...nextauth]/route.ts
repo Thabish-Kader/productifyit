478d9ec7ab2975fd2590548a26dcb3d032796b7f
Thabish <kadertabish2@gmail.com>
2023-06-29 10:36:36 +0400

    patch3
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { authOptions } from "@/app/utils/authOptions";
 import docClient from "@/app/utils/dynamodb";
 import { TCustomer } from "@/types";
 import { PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
@@ -9,67 +10,67 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 	apiVersion: "2022-11-15",
 });
 
-export const authOptions: NextAuthOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-		}),
-	],
-	secret: process.env.NEXTAUTH_SECRET,
-	callbacks: {
-		async session({ session }) {
-			const queryUserParams = {
-				TableName: process.env.TABLE_NAME,
-				KeyConditionExpression: "email = :email",
-				ExpressionAttributeValues: {
-					":email": session.user?.email!,
-				},
-			};
+// export const authOptions: NextAuthOptions = {
+// 	providers: [
+// 		GoogleProvider({
+// 			clientId: process.env.GOOGLE_CLIENT_ID!,
+// 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+// 		}),
+// 	],
+// 	secret: process.env.NEXTAUTH_SECRET,
+// 	callbacks: {
+// 		async session({ session }) {
+// 			const queryUserParams = {
+// 				TableName: process.env.TABLE_NAME,
+// 				KeyConditionExpression: "email = :email",
+// 				ExpressionAttributeValues: {
+// 					":email": session.user?.email!,
+// 				},
+// 			};
 
-			const existingUser = await docClient.send(
-				new QueryCommand(queryUserParams)
-			);
+// 			const existingUser = await docClient.send(
+// 				new QueryCommand(queryUserParams)
+// 			);
 
-			const customers = existingUser.Items as TCustomer[];
+// 			const customers = existingUser.Items as TCustomer[];
 
-			if (customers.length > 0) {
-				session.user!.id = customers[0].id;
-				session.user!.stripeCustomerId = customers[0].id;
-				session.user!.isActive = customers[0].isActive;
-				session.user!.subscriptionId = customers[0].subscriptionId;
-				return session;
-			}
-			try {
-				await stripe.customers
-					.create({
-						email: session.user?.email!,
-						name: session.user?.name!,
-					})
-					.then(async (customer) => {
-						session!.user!.id = customer.id;
-						session!.user!.stripeCustomerId = customer.id;
-						session!.user!.isActive = false;
+// 			if (customers.length > 0) {
+// 				session.user!.id = customers[0].id;
+// 				session.user!.stripeCustomerId = customers[0].id;
+// 				session.user!.isActive = customers[0].isActive;
+// 				session.user!.subscriptionId = customers[0].subscriptionId;
+// 				return session;
+// 			}
+// 			try {
+// 				await stripe.customers
+// 					.create({
+// 						email: session.user?.email!,
+// 						name: session.user?.name!,
+// 					})
+// 					.then(async (customer) => {
+// 						session!.user!.id = customer.id;
+// 						session!.user!.stripeCustomerId = customer.id;
+// 						session!.user!.isActive = false;
 
-						const createUserParmas = {
-							TableName: process.env.TABLE_NAME,
-							Item: {
-								...customer,
-								isActive: false,
-							},
-						};
+// 						const createUserParmas = {
+// 							TableName: process.env.TABLE_NAME,
+// 							Item: {
+// 								...customer,
+// 								isActive: false,
+// 							},
+// 						};
 
-						await docClient.send(new PutCommand(createUserParmas));
-					});
+// 						await docClient.send(new PutCommand(createUserParmas));
+// 					});
 
-				return session;
-			} catch (error) {
-				console.log(error);
-				throw error;
-			}
-		},
-	},
-};
+// 				return session;
+// 			} catch (error) {
+// 				console.log(error);
+// 				throw error;
+// 			}
+// 		},
+// 	},
+// };
 
 const handler = NextAuth(authOptions);
 

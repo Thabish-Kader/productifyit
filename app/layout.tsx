a0948ad6772653ae4b8eb43f419671336c178d93
Thabish <thabish@Thabishs-MacBook-Air.local>
2024-06-01 12:05:47 +0530

    <head/> added
    Signed-off-by: Thabish <thabish@Thabishs-MacBook-Air.local>

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Head>
+		<head>
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			  __html: `
+			  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 			})(window,document,'script','dataLayer','GTM-TT8GKGCQ');
             `,
-          }}
+		}}
         />
-      </Head>
+		</head>
+
 			<Provider>
 				<noscript
           dangerouslySetInnerHTML={{
@@ -43,7 +44,6 @@ export default function RootLayout({
           }}
         />
 				<body className={`${inter.className}  `}>
-
 					{children}</body>
 			</Provider>
 		</html>

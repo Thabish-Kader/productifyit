7e436a599b0fba97ef016037c03f4c0baae8641c
Thabish <thabish@Thabishs-MacBook-Air.local>
2024-06-01 11:50:18 +0530

    gtm added
    Signed-off-by: Thabish <thabish@Thabishs-MacBook-Air.local>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Provider from "./components/Provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -20,8 +21,30 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TT8GKGCQ');
+            `,
+          }}
+        />
+      </Head>
 			<Provider>
-				<body className={`${inter.className}  `}>{children}</body>
+				<noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TT8GKGCQ"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+				<body className={`${inter.className}  `}>
+
+					{children}</body>
 			</Provider>
 		</html>
 	);
